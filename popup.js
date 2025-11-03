a737890944bfb94282e2f634e9db15e48de7d73b
Jubed437 <mahammadjubed213@gmail.com>
2025-11-04 00:07:54 +0530

    Initial Commit

--- popup.js
+++ popup.js
@@ -1,10 +1,7 @@
 document.getElementById("getTitle").addEventListener("click", async () => {
-  // Get the active tab in the current window
+
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  // Extract the title
   const tabTitle = tab.title;
-
-  // Display it in the popup
   document.getElementById("title").textContent = "Tab Title: " + tabTitle;
+  
 });
